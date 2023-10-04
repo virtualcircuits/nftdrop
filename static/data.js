77e126cf923cae3400b01a3f3a45f48f550aad91
Paolo Lozada <35143669+virtualcircuits@users.noreply.github.com>
2023-10-04 09:50:22 +0800

    nftdrop

---
+++ static/data.js
@@ -0,0 +1,90 @@
+export const data = {
+    "program": {
+        "candyMachine": "3qYH7ittmYuYgcWsKtFa7xf7tBDx6P5QNMNjEgdSFSEN",
+        "candyGuard": "5d2QU8bqK2H1PbhTdUa6crEKnsTHaLnJsftSedY4BMC4",
+        "candyMachineCreator": "2oedgNugPNrjpUZ6YbcnEPWVhMvtcvrf7C5wLvN7FL2E",
+        "collectionMint": "7SNUwFCsLQhcmqDdXPTBnuEB1TUmZ6Ppa2PDa99x1nLc"
+    },
+    "items": [
+      {
+        "name": "Number #0001",
+        "image_hash": "0f25cc56a5248acaff1dea34989e04c0a94a321da43e1f806eb49b8551432759",
+        "image_link": "https://arweave.net/jCbQ8b9WJZfcQhliYL5F3ISHOZBoSLGRJpua4JjBydA",
+        "metadata_hash": "e737655253743262ae015ba0479ce58daa9adbfed002cd3cf21963176b553342",
+        "metadata_link": "https://arweave.net/xshNDdu28cih94KDC018jidWQgiRgBESnR3m-zk2rhk",
+        "onChain": true
+      },
+      {
+        "name": "Number #0002",
+        "image_hash": "9461eefa9b19f0a23b166f412d5d0825ea819237eca6f034e2b7bde43aa2b246",
+        "image_link": "https://arweave.net/zYcFw4SaJa-K3pn4DYzir0MgsJRnvdnVTVfx1FySAGU",
+        "metadata_hash": "c71a54941e59b41854d3d3fdf72877fa901c11e2c9cc4cfcff20a552011696aa",
+        "metadata_link": "https://arweave.net/H4ejYuft6aLT9qOpYAoNAIWUs7hLYkPv_3B3BJGgwlw",
+        "onChain": true
+      },
+      {
+        "name": "Number #0003",
+        "image_hash": "33761a265dade9a033a3c8019129c7d258f763255f1f5442e4ab295511be5959",
+        "image_link": "https://arweave.net/q0-QImGCI0laegtGDaJGFpVjhGOX2wJ6lVBUYDUcwBY",
+        "metadata_hash": "962e64f0f8c7e23924485cb8ff0e33f340f58cb7940223e21ab84df6ac20bbb7",
+        "metadata_link": "https://arweave.net/N_byNdvC5Bwa2jesW2hXFWt7nMDpNptgJkRjoWndTUc",
+        "onChain": true
+      },
+      {
+        "name": "Number #0004",
+        "image_hash": "c64f368c086e61e269adab6931548c2edf9c8d1e39320bca6886546a4103af44",
+        "image_link": "https://arweave.net/tx4J7cfd8TXNBX77bs7GVv29FfDgRipVclpwsZjcBSU",
+        "metadata_hash": "add222a6364273902342ce84b377b761cefe29d9d43e0a1972eaa6ed36c7548c",
+        "metadata_link": "https://arweave.net/nhJN0slg8CtSCKRxpUQktq3zik6r42PDpp4jRN-fmJc",
+        "onChain": true
+      },
+      {
+        "name": "Number #0005",
+        "image_hash": "a6ffb1ed11bae54f6ef12e55cfa0cc2dfcf640df25b25f70dfa61cbc5703d12f",
+        "image_link": "https://arweave.net/hw6WyB4AtelVB7VPyL6a76h9sAWGjwQ-YQEj5m-cmYg",
+        "metadata_hash": "0009e21716d872bc5fcb2464f5d1dd99b600dc5e850981596309dd2aff478a2a",
+        "metadata_link": "https://arweave.net/_14HW3f3PRuUYlYGOQNEpNGUelCtGNQGuXZ29KNeOnM",
+        "onChain": true
+      },
+      {
+        "name": "Number #0006",
+        "image_hash": "ed937825cb63f367562580700396d3fc1525c33f4ee507997b2fdf75afcf497a",
+        "image_link": "https://arweave.net/7_sWL7QIHnkxEoqo3cWDEAzVMORmPexCiHOY1t-vzb4",
+        "metadata_hash": "7cfa31023559de77940079b4ad46a46f0fb32a24195fb6398e70172b6a4c01a9",
+        "metadata_link": "https://arweave.net/Q6V3Pwcx6J_gsGauEZPvL0amu8H35zFNOL7Im85AfFk",
+        "onChain": true
+      },
+      {
+        "name": "Number #0007",
+        "image_hash": "08c9bb79481b19ed973c0c412bfe28b626a6ecf79d007b4a900a901dbf84305f",
+        "image_link": "https://arweave.net/I-49jrUzwsciz9cLxC23fczgvHX0lYSS68wp26DBwhE",
+        "metadata_hash": "f45c3fff926cece1bff4c0397541ecf7485688ba3cc2d1805ab1b23c27c96bee",
+        "metadata_link": "https://arweave.net/HynXziCJNrA1w0_b_oTHSNzMud1kqT9S-10Lwr6O6sM",
+        "onChain": true
+      },
+      {
+        "name": "Number #0008",
+        "image_hash": "2f00c788849e7f9995f98d37df98e2c26d6852d9885ea0f3657c64e39bfd773c",
+        "image_link": "https://arweave.net/Ywi20C8v9P8rRw3VrQAKeEpf_6Dipug1odwBP85-AFc",
+        "metadata_hash": "487d10b3e0123100e732f566b3db4a47077c610c6edef9ad4d29b15138a424cd",
+        "metadata_link": "https://arweave.net/R44qcy6hBcj5X6jYEnbGzELZQeqtXwGBVb9EwEReM60",
+        "onChain": true
+      },
+      {
+        "name": "Number #0009",
+        "image_hash": "1752a010fcd73924f2dad2cd6fee3340333c12df0123b50fea038f37284e1ac1",
+        "image_link": "https://arweave.net/CRhogOXrcSOPFSx9MwLXEzqHYSMHFJpwsaBGQYPFVbw",
+        "metadata_hash": "9eceb0e4271b8924fcb9e11b475809bb535e6903693cbd939b54cd77d87d8384",
+        "metadata_link": "https://arweave.net/mXTFChtY1-fd_iKCsTPKnIhsXUxk569--EdPN9CfKsc",
+        "onChain": true
+      },
+      {
+        "name": "Number #0010",
+        "image_hash": "d3872ceb21c300b3b5e3d764a27fcc34a34d65d69dfba3cd597a19c79b0c4bed",
+        "image_link": "https://arweave.net/nN1pkczF-TgKl6dvblOiOY2I7TbDxapEnUUlvCf6pj0",
+        "metadata_hash": "79dcbb3d654756f5890b253461c1c937269002d34a8f7aee27cdbda36c931d61",
+        "metadata_link": "https://arweave.net/gXKkGgXQK_JR0paImdy6ahHPLXumQ1yUD7YtHY884Po",
+        "onChain": true
+      },
+    ]
+}
---
+++ utils/constants.js
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const RPC_ENDPOINT = "https://multi-ultra-moon.solana-devnet.discover.quiknode.pro/1c828c4470ee9a3ddaf5c6e86f58aff3634562bb/";
+
+// Change this with your candy machine id
+export const CANDY_MACHINE_ID = new PublicKey("3qYH7ittmYuYgcWsKtFa7xf7tBDx6P5QNMNjEgdSFSEN");
